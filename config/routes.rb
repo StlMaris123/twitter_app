582cc8082c1f13fa6944c57db560a232d63320f5
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-25 16:21:37 +0300

    static_pages tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/faq'
+  get 'faq' => 'static_pages#faq'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
